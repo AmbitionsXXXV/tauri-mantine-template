56adc3e4ada6ed6fff30bbc37ccfc5c4cc78c9ca
etctera <yilonglei790@gmail.com>
2023-12-28 22:13:20 +0800

    chore: code fmt

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,17 +4,17 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+  format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 #[tauri::command]
 fn sing(name: &str) -> String {
-    format!("ONE OK ROCK {name}")
+  format!("ONE OK ROCK {name}")
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, sing])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![greet, sing])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
