298ac1ee99cf583f4de1e42f71dfc61c9db65817
etctera <yilonglei790@gmail.com>
2023-12-29 16:40:59 +0800

    feat: remove strick mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import Provider from './Provider/Provider'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Provider>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider>
+    <App />
+  </Provider>
 )
