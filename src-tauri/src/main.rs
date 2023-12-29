e558426362da661b19e3b2b8cdbd8250a703c098
etctera <yilonglei790@gmail.com>
2023-12-29 17:44:13 +0800

    feat: auto open devtools when in debug_assertions

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,6 +12,7 @@ pub mod structs;
 
 use commands::fetcher::sing;
 use structs::singer::Singer;
+use tauri::Manager;
 
 fn main() {
   let singer = Singer {
@@ -22,6 +23,12 @@ fn main() {
   println!("{:?}", singer);
 
   tauri::Builder::default()
+    .setup(|app| {
+      if cfg!(debug_assertions) {
+        app.get_window("main").unwrap().open_devtools(); // `main` is the first window from tauri.conf.json without an explicit label
+      }
+      Ok(())
+    })
     .invoke_handler(tauri::generate_handler![greet, sing])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
